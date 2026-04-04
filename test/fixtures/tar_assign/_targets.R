e2a3dc1a04bf8cf0f581b3173bb240808f6aed48
tylermorganwall <tylermw@gmail.com>
2026-04-03 21:57:25 -0400

    -Update tests

--- test/fixtures/tar_assign/_targets.R
+++ test/fixtures/tar_assign/_targets.R
@@ -1,4 +1,15 @@
 tarchetypes::tar_assign({
-  alpha <- tar_target(command = 1)
-  beta <- tar_target(command = alpha + 1)
+	alpha <- tar_target(command = 1)
+	beta <- tar_target(command = alpha + 1)
 })
+
+list(
+	tarchetypes::tar_assign({
+		alpha <- tar_target(command = 1)
+		beta <- tar_target(command = alpha + 1)
+	}),
+	tar_target(
+		gamma,
+		alpha + beta
+	)
+)
